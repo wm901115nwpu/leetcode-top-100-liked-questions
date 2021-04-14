761fe0dbdf1c2e35ac420d5b5cae9131916d022e
wm901115nwpu <wm901115187@sina.com>
2021-04-14 22:08:52 +0800

    Create Problem_0001_TwoSum.cpp

---
+++ Problem_0001_TwoSum.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+std::vector<int> twosum(std::vector<int> nums, int target){
+	std::unordered_map<int, int> m;
+	for (int i = 0; i < nums.size(); ++i) {
+        if (m.count(target - nums[i])) {
+            return {i, m[target - nums[i]]};
+        }
+        m[nums[i]] = i;
+    }
+    return {};
+}
+int main(){
+	// std::vector<int> nums{2,7,11,15};
+	std::vector<int> nums{3, 2, 4};
+	int target = 6;
+	std::vector<int> res = twosum(nums, target);
+	for(auto ans : res){
+		std::cout << ans << std::endl;
+	}
+	
+}
